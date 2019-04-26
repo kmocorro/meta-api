237289c7f75b512a170ec6efa93c1c23dc68a237
kmocorro <kevinmocorro.github@gmail.com>
2019-04-26 08:30:30 +0800

    w

--- controllers/apiLDAP.js
+++ controllers/apiLDAP.js
@@ -10,79 +10,76 @@ let ldap = require('../config').ldap_config;
 module.exports = function(app){
 
     app.post('/api/login', (req, res) => {
+         
+
+        let fields = req.body;
+        console.log(fields);
+    
+        passport.use(setupLDAP());
+
+        passport.initialize();
         
-        let form = new formidable.IncomingForm();
+        passport.authenticate('ldapauth', (err, user, info) => {
+            if(err){
+                res.json({"err": err})
+            } else if(!user){
+                res.json({"err": info.message});
+            } else {
 
-        form.parse(req, (err, fields) => {
-            if(err){return res.send(err)};
+                let token = generateJWT();
 
-            if(fields){
+                console.log(token);
 
-                console.log(fields);
-                
-                passport.use(setupLDAP());
-
-                passport.initialize();
-                passport.authenticate('ldapauth', (err, user, info) => {
-                    if(err){
-                        res.json({"err": err})
-                    } else if(!user){
-                        res.json({"err": info.message});
-                    } else {
-
-                        let token = generateJWT();
-                        
-                        return res.status(200).send({
-                            "token": token
-                        });
-                    }
-
-                    // generateJWT - 
-                    function generateJWT(){
-                        let nickName_array = (user.displayName).split(" ");
-
-                        let token = jwt.sign(
-                            {
-                                id: user.employeeID,
-                                claim: {
-                                    employeeNumber: user.employeeNumber,
-                                    nickName: nickName_array[0],
-                                    displayName: user.displayName,
-                                    title: user.title,
-                                    department: user.department,
-                                    username: user.sAMAccountName
-                                }
-                            }, 
-                            jwt_secret.key
-                        );
-                        return token;
-                    }
-
-                })(req, res);
-
-                // setupLDAP - 
-                function setupLDAP(){
-                    let LDAPSET = {
-                        server: {
-                            url: ldap.url,
-                            bindDN: ldap.bindUser,
-                            bindCredentials: ldap.bindPass,
-                            searchFilter: '(sAMAccountName={{username}})',
-                            searchBase: ldap.searchBase,
-                            connectionTimeout: ldap.connectionTimeout
-                        },
-                        credentialsLookup: function(){
-                            return { username: fields.username , password: fields.password };
-                        }
-                    };
-
-                    let strategy = new LdapStrategy(LDAPSET);
-
-                    return strategy;
-                }
-
+                return res.status(200).send({
+                    "token": token
+                });
             }
-        });
+
+            // generateJWT - 
+            function generateJWT(){
+                let nickName_array = (user.displayName).split(" ");
+
+                let token = jwt.sign(
+                    {
+                        id: user.employeeID,
+                        claim: {
+                            employeeNumber: user.employeeNumber,
+                            nickName: nickName_array[0],
+                            displayName: user.displayName,
+                            title: user.title,
+                            department: user.department,
+                            username: user.sAMAccountName
+                        }
+                    }, 
+                    jwt_secret.key
+                );
+                return token;
+            }
+
+        })(req, res);
+
+        // setupLDAP - 
+        function setupLDAP(){
+            let LDAPSET = {
+                server: {
+                    url: ldap.url,
+                    bindDN: ldap.bindUser,
+                    bindCredentials: ldap.bindPass,
+                    searchFilter: '(sAMAccountName={{username}})',
+                    searchBase: ldap.searchBase,
+                    connectionTimeout: ldap.connectionTimeout
+                },
+                credentialsLookup: function(){
+                    return { username: fields.username , password: fields.password };
+                }
+            };
+
+            let strategy = new LdapStrategy(LDAPSET);
+
+            return strategy;
+        }
+        
+        
 
     });
 
