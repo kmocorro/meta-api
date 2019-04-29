eb28676a652441e8a4aa1f6b21995b245cf6cadf
kmocorro <kevinmocorro.github@gmail.com>
2019-04-29 17:09:05 +0800

    wewewewe

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,8 +3,25 @@ let verifyToken = require('./verifyToken');
 module.exports = function(app){
 
     app.get('/', verifyToken, (req, res) => {
+
+        if(req.userID && req.claim){
+
+            let meta_meta = {
+                title: 'meta/fab4',
+                author: 'kevin mocorro',
+                prototypes: [
+                    {id: 1, protoname: 'COA'},
+                    {id: 2, protoname: 'BRS'},
+                    {id: 3, protoname: 'RMP'},
+                    {id: 4, protoname: 'Engineering Activity'},
+                    {id: 5, protoname: 'Survey App'},
+                ]
+            }
+
+            res.status(200).json(meta_meta);
+        }
         
-        res.send('this is home you have been redirected!');
+        
     });
 
 }
