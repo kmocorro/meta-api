ba5bb80b7b91d78d3736b8f2c72948b7b56a7990
kmocorro <kevinmocorro.github@gmail.com>
2019-10-03 09:09:03 +0800

    removingconsoles

--- controllers/verifyTokenRecertapp.js
+++ controllers/verifyTokenRecertapp.js
@@ -29,12 +29,11 @@ function verifyTokenRecertapp(req, res, next){
                             },  (err, results) => {
                                 if(err){return reject(err)};
         
-                                console.log(results);
                                 if(typeof results[0] !== 'undefined' && results[0] !== null && results.length > 0){
-                                    console.log('hello.. resolved')
+                                    
                                     resolve();
                                 } else {
-                                    console.log('hello.. reject')
+                                
                                     reject();
                                 }
                 
