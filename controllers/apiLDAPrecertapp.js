a1a051c59b80e772976a1650ed004a6c23d1353a
kmocorro <kevinmocorro.github@gmail.com>
2019-10-04 15:47:21 +0800

    awe

--- controllers/apiLDAPrecertapp.js
+++ controllers/apiLDAPrecertapp.js
@@ -26,8 +26,7 @@ module.exports = function(app){
                 res.json({"err": info.message});
             } else {
 
-                let token = generateJWT(user);
-                let jsonToken = {token: token};
+                let examineeProcessList = [];
 
                 function verifyExaminee(){
                     return new Promise((resolve, reject)=>{
@@ -54,9 +53,48 @@ module.exports = function(app){
                     });
                 }
 
+                function examineeProcess(){
+                    return new Promise((resolve, reject) => {
+                        mysql.getConnection((err, connection) => {
+                            if(err){return reject(err)};
+                            // is exists.
+                            connection.query({
+                                sql: 'SELECT * FROM recertapp_examinee_process WHERE employeeNumber = ?',
+                                values: [ user.employeeNumber ]
+                            },  (err, results) => {
+                                if(err){return reject(err)};
+                                //console.log(results);
+                                if(typeof results !== 'undefined' && results !== null && results.length > 0){
+                                    for(i=0;i<results.length;i++){
+                                        examineeProcessList.push({
+                                            processName: results[i].processName
+                                        })
+                                    }
+
+                                    resolve(examineeProcessList);
+                                } else {
+                                    //console.log('hello.. reject')
+                                    reject();
+                                }
+                
+                            })
+                            connection.release();
+                        });
+                    });
+                }
+
                 verifyExaminee().then(() => {
-                    res.cookie('auth_jwt', token); // auth_jwt - authenticated user jsonwebtoken
-                    res.status(200).json(jsonToken);
+                    examineeProcess().then((processName) => {
+
+                        let token = generateJWT(user, processName);
+                        let jsonToken = {token: token};
+
+                        res.cookie('auth_jwt', token); // auth_jwt - authenticated user jsonwebtoken
+                        res.status(200).json(jsonToken);
+
+                    }, (err) => {
+                        res.status(200).json({err: 'You do not have process in the list. Please inform your examiner.'})
+                    })
                 },  (err) => {
                     res.status(200).json({err: 'Login not available for non-examinee.'});
                 })
@@ -65,7 +103,7 @@ module.exports = function(app){
         })(req, res);
 
         // generateJWT - 
-        function generateJWT(user){
+        function generateJWT(user, processName){
             let nickName_array = (user.displayName).split(" ");
 
             let token = jwt.sign(
@@ -77,7 +115,8 @@ module.exports = function(app){
                         displayName: user.displayName,
                         title: user.title,
                         department: user.department,
-                        username: user.sAMAccountName
+                        username: user.sAMAccountName,
+                        processName: processName
                     }
                 }, 
                 jwt_secret.key
